d4b527221ccaab8baf5367db3fb754476acab96a
Bastien Vitour <bastien.vitour@edu.esiee-it.fr>
2024-03-28 17:03:47 +0100

    🚚 Replaced image path

--- src/routes/users/POST_RegisterUser.ts
+++ src/routes/users/POST_RegisterUser.ts
@@ -56,8 +56,7 @@ module.exports = async (req: Request, res: Response) => {
 
         const hashedPassword = await bcrypt.hash(validatedFields.data.password, parseInt(process.env.SALTS!));
 
-        const url = req.protocol + '://' + req.get('host')
-        const profileImg = url + '/uploads' + image.filename
+        const profileImg = '/uploads' + image.filename
 
         await prisma.user.create({
             data: {
