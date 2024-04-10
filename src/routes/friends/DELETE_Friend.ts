746b5ce187f6d7fdd7b5fedd9435667e7d0045d6
julienqtx <julien.quatravaux@live.fr>
2024-04-10 17:27:48 +0200

    deleteFriend

---
+++ src/routes/friends/DELETE_Friend.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+const prisma = new PrismaClient();
+
+module.exports = async (req: Request, res: Response) => {
+	try {
+		const friendshipId = req.params.id;
+
+		if (!friendshipId) {
+			res.status(404).json({ message: "Please give a friendship id" });
+		}
+
+		await prisma.friendship.deleteMany({
+			where: {
+				id: friendshipId
+			}
+		});
+
+		return res.status(200).json({ message: "Friend deleted" });
+	} catch (error: any) {
+		console.log("Error : " + error);
+		return res.status(500).json({ message: "An error occured : " + error });
+	}
+};
